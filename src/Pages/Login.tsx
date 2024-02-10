968860ac773af53df6602e5bc3548435dca64028
Yash dhadve <yashdhadwe@gmail.com>
2024-02-10 13:50:42 +0530

    initial commit

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+    return (
+        <div className="container">
+            <div className="blur">
+                <h1>LOGIN</h1>
+                <div className="login-box">
+                    
+                 </div>
+            </div>
+        </div>
+    )
+}
