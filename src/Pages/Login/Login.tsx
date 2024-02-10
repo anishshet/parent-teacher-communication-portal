6ca69b22d2e82273943ee43be5f4001d8c48dbb5
anishshet <anishshet5@gmail.com>
2024-02-10 15:37:59 +0530

    added login page

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,11 +1,40 @@
+import './style.css'
 export default function Login() {
     return (
-        <div className="container">
-            <div className="blur">
-                <h1>LOGIN</h1>
-                <div className="login-box">
-                    
-                 </div>
+        <div className="container" id="container">
+            <div className="form-container sign-up">
+                <form>
+                    <h1>Create Account</h1>
+                    <span>Use Your Credentials</span>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button>Sign Up</button>
+                </form>
+            </div>
+            <div className="form-container sign-in">
+                <form>
+                    <h1>Sign In</h1>
+                    <span>Use Your Credentials</span>
+                    <input type="email" placeholder="Email or Enrollment ID" />
+                    <input type="password" placeholder="Password" />
+                    <a href="#">Forgot Your Password?</a>
+                    <button>Sign In</button>
+                </form>
+            </div>
+            <div className="toggle-container">
+                <div className="toggle">
+                    <div className="toggle-panel toggle-left">
+                        <h1>Welcome Back!</h1>
+                        <p>Enter your personal details to use all of site features</p>
+                        <button className="hidden" id="login">Sign In</button>
+                    </div>
+                    <div className="toggle-panel toggle-right">
+                        <h1>Hello!</h1>
+                        <p>Register with your personal details to use the site</p>
+                        <button className="hidden" id="register">Sign Up</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
